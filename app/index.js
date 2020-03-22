e988cb619d7e8e94a799462e9beb9053bfa16597
Andrey Aratov <beg431@yandex.ru>
2020-03-22 12:23:21 +0300

    added header, footer components

--- app/index.js
+++ app/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './index.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './css/styles.css';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>Hello world</div>
+			<React.Fragment>
+				<Header/>
+				<main className="layout page__content">
+					<div className="layout__container">
+						<h2>Hello world</h2>
+					</div>
+				</main>
+				<Footer/>
+			</React.Fragment>
 		)
 	}
 }
